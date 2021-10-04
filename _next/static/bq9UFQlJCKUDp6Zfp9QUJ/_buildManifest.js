14d8f84cfdbb3c855032aa52b5a31937376a62d7
ramjak <rama.jakaria@gmail.com>
2021-10-04 10:46:43 +0000

    Deploying to gh-pages from @ ramjak/ramjak.github.io@369f7fd271d4948a94fab69e110c920cadaba715 🚀

---
+++ _next/static/bq9UFQlJCKUDp6Zfp9QUJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/546-10da1539d90a1c4026e5.js","static/css/61472729c782d52dead7.css","static/chunks/pages/index-e6e52c38058f81c7544e.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
