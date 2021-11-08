cc989a17a114ce7d7fe374353b8a5ee00aff9a80
ramjak <rama.jakaria@gmail.com>
2021-11-08 21:50:06 +0700

    feat: add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import {getYearDiff} from "../utils/date";
 
 class MyDocument extends Document {
   public render() {
@@ -9,6 +10,9 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap"
             rel="stylesheet"
           />
+          <meta name="description" content={`Personal portfolio of Rama Jakaria, a web developer based in Jakarta with more than ${getYearDiff("1 April 2016")} years of experience in front-end and back-end.`} />
+          <meta name="keyword" content="rama, jakaria, web developer, front end developer" />
+          <meta name="referrer" content="same-origin" />
         </Head>
         <body>
           <Main />
